fac95d8a106fd93a0d847a544a2a3ea72513684d
Javier-Git <xavi_uroot@outlook.com>
2020-08-03 19:55:01 -0500

    Upload docs

---
+++ docs/precache-manifest.34910e40f4a39b05349441d482acc7b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "654a97b3d04e81a7b3b9a0ba1cd67fc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53c687a348f9a811e9c0",
+    "url": "/static/css/main.4c798faf.chunk.css"
+  },
+  {
+    "revision": "e2cfa34183aaf241a1c0",
+    "url": "/static/js/2.e331d3b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e331d3b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53c687a348f9a811e9c0",
+    "url": "/static/js/main.5915ce30.chunk.js"
+  },
+  {
+    "revision": "9078284b3c5a7cc4441c",
+    "url": "/static/js/runtime-main.3c7fdb96.js"
+  }
+]);
