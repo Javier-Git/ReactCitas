acae4740a6472deb941fa0b8c6cba517c32cc260
Javier-Git <xavi_uroot@outlook.com>
2020-08-03 20:10:11 -0500

    Updates

---
+++ precache-manifest.e9d3aa4bd0d71fa25b6422365060164f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "defc11bdf1a971396edf89be0ee7a9b4",
+    "url": "/ReactCitas/index.html"
+  },
+  {
+    "revision": "70e0f4eb3d11f53b9918",
+    "url": "/ReactCitas/static/css/main.4c798faf.chunk.css"
+  },
+  {
+    "revision": "e2cfa34183aaf241a1c0",
+    "url": "/ReactCitas/static/js/2.e331d3b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactCitas/static/js/2.e331d3b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70e0f4eb3d11f53b9918",
+    "url": "/ReactCitas/static/js/main.0385b7b9.chunk.js"
+  },
+  {
+    "revision": "5d701e77f967cce4cffa",
+    "url": "/ReactCitas/static/js/runtime-main.8618b118.js"
+  }
+]);
